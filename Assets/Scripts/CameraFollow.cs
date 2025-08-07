b192446e3a8a5dc03199009d830d9f1108f88496
MihailKanev01 <mihail.kanev.1995@abv.bg>
2025-08-07 19:31:26 +0300

    Small changes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 3, -6);
+    public float smoothSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
+    public float mouseSensitivity = 100f;
+    private float xRotation = 0f;
+
+    void LateUpdate()
     {
-        
+        if (target == null) return;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -35f, 60f);
+
+        transform.position = target.position + offset;
+        transform.RotateAround(target.position, Vector3.up, mouseX);
+        Quaternion rotation = Quaternion.Euler(xRotation, transform.eulerAngles.y, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, smoothSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public Transform cam;
+    private Animator animator;
+    private CharacterController controller;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        bool hasInput = direction.magnitude >= 0.1f;
+        if (animator != null)
+        {
+            animator.SetBool("isRunning", hasInput);
+        }
+
+        if (hasInput)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
     }
 }
